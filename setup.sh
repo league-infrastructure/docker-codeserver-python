e0f19a0e1c1ecf0be8330899b05cf9a96c5a0aed
Eric Busboom <eric@civicknowledge.com>
2025-01-19 15:54:22 -0800

    .

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-pip3 install --upgrade pip
-pip3 install  -r requirements.txt
 
-git config --global pull.rebase true
 
 echo "export PYTHONPATH=$(pwd)/.lib/:$PYTHONPATH" >> ~/.zshrc
 echo "export PYTHONPATH=$(pwd)/.lib/:$PYTHONPATH" >> ~/.bashrc
