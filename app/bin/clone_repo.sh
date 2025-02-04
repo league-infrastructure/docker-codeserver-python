0a5d3d0ad20a9074de8a06c2d159ba61cbfa2372
Eric Busboom <eric@civicknowledge.com>
2025-02-04 11:00:19 -0800

    Release version 0.3.4=5

--- app/bin/clone_repo.sh
+++ app/bin/clone_repo.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
 echo "Cloning repository..."
+
+cd /workspace
+
+if [ -d "$(basename "$INITIAL_GIT_REPO" .git)" ]; then
+    echo "Repository already exists."
+    exit 1
+fi
+
+git clone "$INITIAL_GIT_REPO"
+
